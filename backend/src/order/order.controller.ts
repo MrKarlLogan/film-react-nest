7ad771da79afa85c644d6bc7c2d34a7d93d851aa
MrKarlLogan <mrelectricguitar7@gmail.com>
2026-02-05 13:07:24 +0300

    Реализовано создание заказ

--- backend/src/order/order.controller.ts
+++ backend/src/order/order.controller.ts
@@ -1,7 +1,17 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { NewOrderDTO, PostOrderDTO } from './dto/order.dto';
+import { OrderService } from './order.service';
 
 @Controller('order')
 export class OrderController {
+  constructor(private orderService: OrderService) {}
+
   @Post()
-  createOrder() {}
+  async createOrder(@Body() body: PostOrderDTO): Promise<NewOrderDTO> {
+    const orders = await this.orderService.newOrders(body.tickets);
+    return <NewOrderDTO>{
+      total: orders.length,
+      items: orders,
+    };
+  }
 }
